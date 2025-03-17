39f450cbe73551e8cb769862716b8fd603ac3e54
Dominic Venetucci <dominic.venetucci@dycominc.com>
2025-03-17 09:16:06 -0400

    updated 03.17.2025

--- JSON_to_CSV.py
+++ JSON_to_CSV.py
@@ -1,131 +1,138 @@
 import pandas as pd
+import numpy as np
 import json
 import PySimpleGUI as sg
 from pandas import json_normalize
 from pathlib import Path
+from itertools import chain
+import argparse
+
 
 # p = Path(r'c:/Users/DV0095/Documents/Python_Projects/JSON_to_CSV/JSON_FILES/Google_JSON.json') #JSON file Path/Directory
 
 # with p.open('r', encoding='utf-8') as f:
     # data = json.load(f) #reads the JSON file
 
-def main():
-    sg.theme('PythonPlus')
+def main(): #GUI Window
+    parser = argparse.ArgumentParser(description="Destinguish between GUI and command line")
+    parser.add_argument("-v", "--pysimple", action="store_true", help="Use the GUI if you have PySimpleGUI")
+    parser.add_argument("-c", "--command", type=str, help="Specify the JSON file path for command line mode")
+    args = parser.parse_args()
 
-    layout = [[sg.T("")],
-                [sg.Text("Upload the JSON version of the Interpretation File: "), sg.Input(key="file_path"), sg.FileBrowse(key="file_path_browse")],
-                [sg.T("")],
-                [sg.Button("Submit", bind_return_key=True), sg.Button('Cancel')]]
+    if args.pysimple:
+        
+        sg.theme('PythonPlus')
 
-    window = sg.Window('Main Menu', layout, size=(800, 180))
+        layout = [[sg.T("")],
+                    [sg.Text("Upload the downloaded Tree Dialogue JSON File: "), sg.Input(key="file_path"), sg.FileBrowse(key="file_path_browse")],
+                    [sg.T("")],
+                    [sg.Button("Submit", bind_return_key=True), sg.Button('Cancel')]]
 
-    while True:
-        event, values = window.Read()
-        if event == sg.WIN_CLOSED or event == 'Cancel':
-            break
-        elif event == 'Submit':
-            json_file = values['file_path']
-            window.close()
+        window = sg.Window('Main Menu', layout, size=(800, 180))
 
+        while True:
+            event, values = window.Read()
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                break
+            elif event == 'Submit':
+                json_file = values['file_path']
+                window.close()
 
-    json_file_interp = json.load(open(json_file))
+        data = json.load(open(json_file))
 
+    elif args.command:
 
-    def add_definitions_key(interp):
-        outcomes = interp['outcomes']
-        for outcome in outcomes:
-            if 'definitions' not in outcome and 'attributes' in outcome:
-                attributes = outcome.get('attributes', {})
-                conditions = outcome.get('conditions', [])
-                outcome['definitions'] = [
-                    {
-                        'attributes': attributes,
-                        'conditions': conditions
-                    }
-                ]
-            if 'definitions' not in outcome and 'attributes' in outcome:
-                attributes = outcome.get('attributes', {})
-                outcome['definitions'] = [
-                    {
-                        'attributes': attributes
-                    }
-                ]
-            if 'definitions' not in outcome and 'conditions' in outcome:
-                conditions = outcome.get('conditions', [])
-                outcome['definitions'] = [
-                    {
-                        'conditions': conditions
-                    }
-                ]
-                outcome.pop('attributes', None)  # Remove 'attributes' key
-                outcome.pop('conditions', None)  # Remove 'conditions' key
-        return interp
+        p = Path(r'c:\Users\DV0095\Downloads\20250304141748.json') #JSON file Path/Directory
+        # Read the JSON file
+        with p.open('r', encoding='utf-8') as file:
 
-    data = add_definitions_key(json_file_interp)
-
-    df = pd.json_normalize(data['outcomes'], meta = ['name'], record_path=['definitions']) #flattens JSON data into a dataframe. Most notably flattens the ['definitions'] dictionary in the JSON file. Turns out all cwi's need 'definitions'
-    df.fillna('', inplace=True) #removes all NaN values in the csv file/helps clear empty spaces when combining column strings. The difference between null and ''.
+            data = json.load(file)
 
 
-    if any(col.startswith('variables.') for col in df.columns):
-        variables_cols = [col for col in df.columns if col.startswith('variables.')]
-        df['combined_variables'] = df[variables_cols].apply(lambda row: '\n'.join(f"{col.split('.')[1]}: {value}" for col, value in row.iteritems() if value != ''), axis=1)
-        df = df.drop(variables_cols, axis=1)
+    # def add_definitions_key(interp):
+    #     outcomes = interp['outcomes']
+    #     for outcome in outcomes:
+    #         if 'definitions' not in outcome and 'attributes' in outcome:
+    #             attributes = outcome.get('attributes', {})
+    #             conditions = outcome.get('conditions', [])
+    #             outcome['definitions'] = [
+    #                 {
+    #                     'attributes': attributes,
+    #                     'conditions': conditions
+    #                 }
+    #             ]
+    #         if 'definitions' not in outcome and 'attributes' in outcome:
+    #             attributes = outcome.get('attributes', {})
+    #             outcome['definitions'] = [
+    #                 {
+    #                     'attributes': attributes
+    #                 }
+    #             ]
+    #         if 'definitions' not in outcome and 'conditions' in outcome:
+    #             conditions = outcome.get('conditions', [])
+    #             outcome['definitions'] = [
+    #                 {
+    #                     'conditions': conditions
+    #                 }
+    #             ]
+    #         #outcome.pop('attributes', None)  # Remove 'attributes' key
+    #         #outcome.pop('conditions', None)  # Remove 'conditions' key
+    #     return interp
+
+    # data = add_definitions_key(json_file_interp)
+
+
+    df = pd.json_normalize(data['outcomes'], meta = ['name'])
+    df.replace({pd.NA: '', np.nan: ''}, inplace=True)
+    df_exploded = df.explode('definitions', ignore_index=True)
+
+    # Extract 'attributes' and 'conditions' from each dictionary
+    df_exploded['def-attributes'] = df_exploded['definitions'].apply(lambda x: x['attributes'] if isinstance(x, dict) and 'attributes' in x else None)
+    df_exploded['def-conditions'] = df_exploded['definitions'].apply(lambda x: x['conditions'] if isinstance(x, dict) and 'conditions' in x else None)
+
+    if any(col.startswith('variables.') for col in df_exploded.columns):
+        variables_cols = [col for col in df_exploded.columns if col.startswith('variables.')]
+        df_exploded['combined_variables'] = df_exploded[variables_cols].apply(lambda row: '\n'.join(f"{col.split('.')[1]}: {value}" for col, value in row.items() if value != ''), axis=1)
+        df_exploded = df_exploded.drop(variables_cols, axis=1)
         pass
     else:
         # No 'variables.' columns exist
         pass
 
-    if any(col.startswith('attributes.') for col in df.columns):
-        #df= df.drop('attributes.quantity.type')
-        attributes_cols = [col for col in df.columns if col.startswith('attributes.') and col != 'attributes.quantity.type']
-        df['combined_attributes'] = df[attributes_cols].apply(lambda row: '\n'.join(f"{col.split('.')[1]}: {value}" for col, value in row.iteritems() if value != ''), axis=1)
-        df = df.drop(attributes_cols, axis=1)# Drop original 'attributes.' columns
+    if any(col.startswith('attributes.') for col in df_exploded.columns):
+
+        attributes_cols = [col for col in df_exploded.columns if col.startswith('attributes.') and col != 'attributes.quantity.type']
+        df_exploded['combined_attributes'] = df_exploded[attributes_cols].apply(lambda row: '\n'.join(f"{col.split('.')[1]}: {value}" for col, value in row.items() if value != ''), axis=1)
+        df_exploded = df_exploded.drop(attributes_cols, axis=1)# Drop original 'attributes.' columns
         pass
     else: 
         pass
-
-
-    #df['combined_attributes'] = df.apply(lambda row: '\n'.join(f"{key.split('.')[1]}: {value}" for key, value in row.iteritems() if key.startswith('attributes.') and value != ''), axis=1)
-    #drop any unneeded columns. Possibly contingent on the tree JSON file
-    #df.drop(['attributes.quantity.question','attributes.quantity','attributes.quantity.operation','attributes.quantity.args'], axis=1, inplace=True)
 
     ### Organization ###
-    if any(col.startswith('combined_variables') for col in df.columns):
-    # Define the desired column order
-        column_order = ['name','combined_variables', 'combined_attributes', 'conditions'] #column order may be contingent on the tree JSON file
-    # Reorder the columns using reindex
-        df = df.reindex(columns=column_order)
-    # change the names of any of the columns *must be in the order layout above*
-        df.columns = ['CWI_UNIT Names', 'Variables', 'Attributes', 'Conditions']
-        df.reset_index(drop=True, inplace=True)
+    if any(col.startswith('combined_variables') for col in df_exploded.columns):
+        column_order = ['name','combined_variables', 'combined_attributes', 'conditions', 'def-attributes', 'def-conditions'] #column order may be contingent on the tree JSON file
+        df_exploded = df_exploded.reindex(columns=column_order)
+
+        df_exploded.columns = ['CWI_UNIT Names', 'Variables', 'Attributes', 'Conditions', 'Def-Attributes', 'Def-Conditions']
+        df_exploded.reset_index(drop=True, inplace=True)
     else: 
-        column_order = ['name', 'combined_attributes', 'conditions'] #column order may be contingent on the tree JSON file
-        df = df.reindex(columns=column_order)
-        df.columns = ['CWI_UNIT Names', 'Attributes', 'Conditions']
-        df.reset_index(drop=True, inplace=True)
+        column_order = ['name', 'combined_attributes', 'conditions', 'def-attributes', 'def-conditions'] #column order may be contingent on the tree JSON file
+        df_exploded = df_exploded.reindex(columns=column_order)
+        df_exploded.columns = ['CWI_UNIT Names', 'Attributes', 'Conditions', 'Def-Attributes', 'Def-Conditions']
+        df_exploded.reset_index(drop=True, inplace=True)
+
+
 
     #### Remove extra cwi names ####
-    df.loc[(df['CWI_UNIT Names'].duplicated()) & (df['CWI_UNIT Names'] != 'UNIT_CODE'), 'CWI_UNIT Names'] = ''
-
+    #df.loc[(df['CWI_UNIT Names'].duplicated()) & (df['CWI_UNIT Names'] != 'UNIT_CODE'), 'CWI_UNIT Names'] = ''
 
 
     # print new csv file and use Tree interp name for organization.
-    df.to_csv('ANSCO_ATT_SVA_Tree.csv') #NAME THE FILE TO THE TREE YOU ARE USING. Contructs and saves the data frame into a csv file. Must have a directory location in terminal.
-    #print(df)  ##prints a view of what the data frame will look like. View/Spawns/Populates in terminal, might not be all of the info if JSON file is large.
+    df_exploded.to_csv('Ubiquity_02_10_2025.csv')
+
+
+    #print(df_exploded)  ##prints a view of what the data frame will look like. View/Spawns/Populates in terminal, might not be all of the info if JSON file is large.
 
 if __name__=='__main__':
     main()
 
-
-
-
-#### 1) remove index column 1.1) SUCCESS! hide unused columns 
-# df.index.name = 'Index'
-
-#### 2) remove row duplicates/remove row indecies (mainly for cwi_names) 
-#### 3) SUCCESS! DONE! combine the quantity.operations and args columns in ['quantity']
-#### 4) SUCCESS! move column values so that it is more organized. IE. cwi_name, attributes, conditions.
-#### 5) maybe skip lines so the information is not so bunched??
-
-# SUCCESS all keys need a definitons value!! Fix when you have the time. Fixed with the function.
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# JSON_to_CSV
-A python script that will convert a JSON file to CSV so that it will be easily read be those who do not wish to read code. There are specific parameters implemented that will organize the CSV. The scripts main use is to organize and count how many unit_codes are in an interpretation file. Also used to review if some units are missing and need to be added per contract updates.
+# JSON to CSV
+
+This program will create a visual friendly CSV/Excel file from an interpretation file. :smiley:
+
+# Inspiration 
+Anyone who does not read programming languages on a regular bases may find reading JSON/YAML - even for being a human-read-able text language - to be intimidating. The information stored in our data files (decision tree dialogues) is cluttered with lists and dictionaries. Most of the time, not all the information stored is relevant to the cause. This program was built to pull and to organize the necessary information for the people on our team and for any subsidiary users. The format of CSV/Excel files are used and interprated universally. 
+
+# Installation 
+git clone the repository. 
+
+Packages:
+```
+import pandas as pd
+import numpy as np
+import json
+import PySimpleGUI as sg
+from pandas import json_normalize
+from pathlib import Path
+from itertools import chain
+import argparse
+```
+
+You can run the program using PySimpleGUI if license installed. Please see **[run w/ PySimpleGUI](#run-w-pysimplegui)** for instructions on running the program using the GUI and visit **[Licenses](#licenses)** to learn how to acquire PySimpleGUI. 
+
+or 
+
+You can run the program through the command line by assigning the file location in variable 'p' on line 39. Reference **[run w/ file location](#run-w-file-location)**
+
+## run w/ PySimpleGUI
+Run the program by using: `py Dialogue_Reader.py -v`
+
+Input a JSON file into the GUI window by clicking 'Browse'. The GUI will allow you to browse your computer files. Select the 'Tree_Dialogue_JSON_File.json' you wish to convert to CSV. Click 'Submit' and the new CSV file will be in your files where you set the directory in the terminal.
+
+## run w/ file location
+Run the program by using: `py Dialogue_Reader.py -c p`
+
+The variable `p` should be assigned the location where the JSON file exists in the file path/directory. 
+
+`p = Path(r'c:/Users/DV0095/Documents/Python_Projects/dialogues/test.json')`
+
+The new CSV file will be in your files where you set the directory in the terminal.
+
+## run/error notes
+You will need a 'definitions' list somewhere in the interpretation file to run this program. Most if not all interpretation files will have a 'definitions' list but this is a warning.
+
+## Licenses
+Submit an Altas Request to get a developer key for the PySimpleGUI library. 'Request New Approved Software'. The library should be an option to select under the approved softwares.
+
+Add developer key: Run the program with PySimpleGUI (**run w/ PySimpleGUI**) and the interface will prompt you to copy and paste the key into a text box. I believe a new key will be needed one calendar year after purchase.
+
+
+# Name the CSV 
+You can enter the name of your csv file on line 131.
+
+`df_exploded.to_csv('include_name.csv')`
+
+
+Replace 'include_name' with any name you would like to assign the CSV file for further uniqueness.
+
+# Visuals
+
+Decision tree interpretation files look like this.
+![decision tree interpretation files look like this.](Visual_References/interp_json.png)
+
+Example output of the program.
+![Example output of the program.](Visual_References/CSV_output.png)
+
